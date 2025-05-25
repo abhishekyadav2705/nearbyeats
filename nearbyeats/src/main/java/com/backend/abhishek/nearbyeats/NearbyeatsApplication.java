a72ad9decb1dad77eb3f6c6261f25db0c7c93a73
Abhishek Yadav <abhishekyadav84595@gmail.com>
2025-05-25 13:38:26 +0530

    initial commit

---
+++ nearbyeats/src/main/java/com/backend/abhishek/nearbyeats/NearbyeatsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.abhishek.nearbyeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyeatsApplication.class, args);
+	}
+
+}
